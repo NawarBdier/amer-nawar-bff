e1c9b2038cc684f0fe9f3f8037d1371f2385ddaa
Nawar Bdier <nawar.bdier@stud.th-deg.de>
2021-07-03 19:08:59 +0200

    initial

---
+++ code.py
@@ -0,0 +1,12 @@
+def matricalation(ID):
+    sum =0
+    for x in ID:
+        sum += int(x)
+    return sum
+
+
+amer_ID = input("Enter your matriculation number is = ")
+nawar_ID = input("Enter your matriculation number is = ")
+sum2 = matricalation(nawar_ID)
+sum1 = matricalation(amer_ID)
+print(sum1-sum2 - 25)
