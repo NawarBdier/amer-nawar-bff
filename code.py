15a17d4503f570df1df238b0068342bfe46e9e18
Amer Alsahwabkeh <Amer1alshawabkeh1@gmail.com>
2021-07-03 20:02:13 +0200

    Nwaaara

--- code.py
+++ code.py
@@ -5,10 +5,10 @@ def matricalation(ID):
     return sum
 
 
-amer_ID = input("Enter your matriculation number is = ")
-nawar_ID = input("Enter your matriculation number is = ")
+amer_ID = input("Enter your matriculation number here = ")
+nawar_ID = input("Enter your matriculation number here = ")
 sum2 = matricalation(nawar_ID)
 sum1 = matricalation(amer_ID)
-print(sum1-sum2)
+print(sum1/sum2)
 
 # Could you please commit you edit?
