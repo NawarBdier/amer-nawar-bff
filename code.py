a42b234d83df09a785a50ef2af8a87bfebb18781
Amer Alsahwabkeh <Amer1alshawabkeh1@gmail.com>
2021-07-03 19:45:38 +0200

    negative

--- code.py
+++ code.py
@@ -9,6 +9,6 @@ amer_ID = input("Enter your matriculation number is = ")
 nawar_ID = input("Enter your matriculation number is = ")
 sum2 = matricalation(nawar_ID)
 sum1 = matricalation(amer_ID)
-print(sum1+sum2)
+print(sum1-sum2)
 
 # Could you please commit you edit?
