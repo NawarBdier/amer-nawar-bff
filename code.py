d8a8f889a8aaea44bdf986dd959314145d6a85e6
Amer Alsahwabkeh <Amer1alshawabkeh1@gmail.com>
2021-07-03 20:09:20 +0200

    find the conflict

--- code.py
+++ code.py
@@ -5,8 +5,8 @@ def matricalation(ID):
     return sum
 
 
-amer_ID = input("Enter your matriculation number here = ")
-nawar_ID = input("Enter your matriculation number here = ")
+amer_ID = input("Enter your matriculation number here ")
+nawar_ID = input("Enter your matriculation number here")
 sum2 = matricalation(nawar_ID)
 sum1 = matricalation(amer_ID)
 print(sum1/sum2)
